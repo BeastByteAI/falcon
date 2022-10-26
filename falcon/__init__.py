511d19eec19dad46db2accaf1790dfec9337abea
Iryna K <iryna230520@gmail.com>
2022-10-26 14:41:09 +0200

    disabled warnings

--- falcon/__init__.py
+++ falcon/__init__.py
@@ -1,4 +1,9 @@
 __version__ = '0.2.2'
 __author__ = 'Oleg Kostromin, Marco Pasini, Iryna Kondrashchenko'
 
+from faulthandler import disable
+
 from falcon.main import initialize, AutoML
+from falcon.utils import disable_warnings
+
+disable_warnings()
