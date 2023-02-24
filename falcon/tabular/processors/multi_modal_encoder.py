01147324724295b9a977d009d920f42c63d26d6a
Oleg Kostromin <kostromin97@gmail.com>
2023-02-24 15:47:02 +0100

    tests mm encoder

--- falcon/tabular/processors/multi_modal_encoder.py
+++ falcon/tabular/processors/multi_modal_encoder.py
@@ -1,28 +1,49 @@
 import numpy as np
 from numpy import typing as npt
 from falcon.types import Float32Array, ColumnTypes
-from sklearn.compose import ColumnTransformer
 from typing import List, Optional, Type, Any
-from sklearn.base import BaseEstimator
+from sklearn.compose import ColumnTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline as SKLPipeline
-from sklearn.preprocessing import MaxAbsScaler, OrdinalEncoder
+from sklearn.preprocessing import MaxAbsScaler
 from skl2onnx.sklapi import CastTransformer
 from falcon.tabular.processors.scaler_and_encoder import ScalerAndEncoder
 from falcon.addons.sklearn.preprocessing.date_tokenizer import DateTimeTokenizer
+from falcon.addons.sklearn.decomposition.svd import ConditionalSVD
+
 
 class MultiModalEncoder(ScalerAndEncoder):
-    def _get_date_tokenizer(self, ct: ColumnTypes) -> BaseEstimator:
+    def _get_date_tokenizer(self, ct: ColumnTypes) -> SKLPipeline:
         if ct == ColumnTypes.DATE_YMD_ISO8601:
             f = r"%Y-%m-%d"
         elif ct == ColumnTypes.DATETIME_YMDHMS_ISO8601:
             f = r"%Y-%m-%dT%H:%M:%SZ"
-        else: 
+        else:
             raise ValueError("Unknown column type encountered")
         return SKLPipeline(
             steps=[
                 ("date_tokenizer", DateTimeTokenizer(format=f)),
                 ("cast32", CastTransformer()),
                 ("sc", MaxAbsScaler()),
+            ]
+        )
+
+    def _get_text_tfidf(self) -> SKLPipeline:
+        return SKLPipeline(
+            steps=[
+                (
+                    "tfidf_vectorizer",
+                    TfidfVectorizer(
+                        stop_words="english",
+                        input="content",
+                        analyzer="word",
+                        max_features=1024,
+                        token_pattern = "[a-zA-Z0-9_]+"
+
+                    ),
+                ),
+                ("cast32", CastTransformer()),
+                ("svd", ConditionalSVD(n_components=32)),
             ]
         )
 
@@ -45,13 +66,17 @@ class MultiModalEncoder(ScalerAndEncoder):
                 method = self._get_numeric_scaler()
             elif v in [
                 ColumnTypes.DATE_YMD_ISO8601,
-                ColumnTypes.DATETIME_YMDHMS_ISO8601
+                ColumnTypes.DATETIME_YMDHMS_ISO8601,
             ]:
                 method = self._get_date_tokenizer(v)
+            elif v == ColumnTypes.TEXT_UTF8:
+                method = self._get_text_tfidf()
             else:
                 method = self._get_ordinal_encoder()
-            t = (f"input {i}", method, [i])
+            if v != ColumnTypes.TEXT_UTF8:
+                t = (f"input {i}", method, [i])
+            else: 
+                t = (f"input {i}", method, i)
             transformers.append(t)
-
         self.ct = ColumnTransformer(transformers)
         self.ct.fit(X)
--- falcon/tabular/processors/scaler_and_encoder.py
+++ falcon/tabular/processors/scaler_and_encoder.py
@@ -17,14 +17,13 @@ from sklearn.preprocessing import MaxAbsScaler, OrdinalEncoder
 from skl2onnx.sklapi import CastTransformer
 from falcon.serialization import SerializedModelRepr
 
+
 class ScalerAndEncoder(Processor, ONNXConvertible):
     """
     Applies OneHotEncoder/OrdinalEncoder on low/high cardinality categorical features and StandardScaler on numerical features.
     """
-    
-    def __init__(
-        self, mask: List[ColumnTypes]
-    ) -> None: 
+
+    def __init__(self, mask: List[ColumnTypes]) -> None:
         """
         Parameters
         ----------
@@ -32,22 +31,38 @@ class ScalerAndEncoder(Processor, ONNXConvertible):
             provides a type for each column at a given index
         """
         self.mask = mask
-    
+
     def _get_ohe(self) -> BaseEstimator:
-        if version.parse(sklearn_version) < version.parse('1.2.0'):
+        if version.parse(sklearn_version) < version.parse("1.2.0"):
             not_sparse = {"sparse": False}
-        else: 
+        else:
             not_sparse = {"sparse_output": False}
-        method = OneHotEncoder(
-                    categories="auto", handle_unknown="ignore", **not_sparse
-                )
+        method = OneHotEncoder(categories="auto", handle_unknown="ignore", **not_sparse)
         return method
 
     def _get_numeric_scaler(self) -> BaseEstimator:
-        return SKLPipeline(steps = [('cast64', CastTransformer(dtype=np.float64)),('scaler', StandardScaler(with_mean=True, with_std=True)),('cast32', CastTransformer())])
+        return SKLPipeline(
+            steps=[
+                ("cast64", CastTransformer(dtype=np.float64)),
+                ("scaler", StandardScaler(with_mean=True, with_std=True)),
+                ("cast32", CastTransformer()),
+            ]
+        )
 
     def _get_ordinal_encoder(self) -> BaseEstimator:
-        return SKLPipeline(steps=[('ord_enc', OrdinalEncoder(categories="auto", handle_unknown = "use_encoded_value", unknown_value = -1)), ('sc', MaxAbsScaler())])
+        return SKLPipeline(
+            steps=[
+                (
+                    "ord_enc",
+                    OrdinalEncoder(
+                        categories="auto",
+                        handle_unknown="use_encoded_value",
+                        unknown_value=-1,
+                    ),
+                ),
+                ("sc", MaxAbsScaler()),
+            ]
+        )
 
     def fit(self, X: npt.NDArray, y: Any = None, *args: Any, **kwargs: Any) -> None:
         """
@@ -61,7 +76,7 @@ class ScalerAndEncoder(Processor, ONNXConvertible):
             dummy argument to keep compatibility with pipeline training
         """
         transformers = []
-        
+
         for i, v in enumerate(self.mask):
             if v == ColumnTypes.CAT_LOW_CARD:
                 method = self._get_ohe()
@@ -109,7 +124,9 @@ class ScalerAndEncoder(Processor, ONNXConvertible):
         """
         return Float32Array
 
-    def forward(self, X: npt.NDArray[np.object_], *args: Any, **kwargs: Any) -> npt.NDArray:
+    def forward(
+        self, X: npt.NDArray[np.object_], *args: Any, **kwargs: Any
+    ) -> npt.NDArray:
         """
         Equivalent of `.predict()` or `.transform()`.
 
@@ -128,7 +145,7 @@ class ScalerAndEncoder(Processor, ONNXConvertible):
 
     def to_onnx(self) -> SerializedModelRepr:
         """
-        Serializes the encoder to onnx. 
+        Serializes the encoder to onnx.
         Each feature in the original dataset is mapped to its own input node (`float32` for numerical or `string` for categorical).
 
         Returns
@@ -151,7 +168,10 @@ class ScalerAndEncoder(Processor, ONNXConvertible):
             convert_sklearn(
                 self.ct,
                 initial_types=initial_types,
-                target_opset={'': ONNX_OPSET_VERSION, 'ai.onnx.ml': ML_ONNX_OPSET_VERSION},
+                target_opset={
+                    "": ONNX_OPSET_VERSION,
+                    "ai.onnx.ml": ML_ONNX_OPSET_VERSION,
+                },
                 options={StandardScaler: {"div": "div_cast"}},
             ),
             len(self.mask),
